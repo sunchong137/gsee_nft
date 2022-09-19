6b6d7ddec61d1e018c46267f08c3732ff46ea08c
sunchong137 <sunchong137@gmail.com>
2022-09-19 13:32:41 -0400

    helpers test wrting

--- gsee/helpers.py
+++ gsee/helpers.py
@@ -3,18 +3,22 @@ from scipy.special import eval_chebyt
 from scipy import integrate
 
 
-def dft_coeffs_approx_heaviside(max_dft_order, rescaled_energy_acc, dft_grid, nmesh=100):
+def dft_coeffs_approx_heaviside(max_dft_order, rescaled_energy_acc, dft_grids, nmesh=2000):
     """
-    Evaluate the Fourier coefficients the approximate heaviside function.
+    Evaluate the Fourier coefficients the approximate heaviside function at dft_grids.
     Args:
         max_dft_order       : int, cutoff of the DFT expansion (d in the paper).
         rescaled_energy_acc : float, energy accuracy * tau (delta in the paper).  
-        dft_grid            : int or array of int, the order(s) at which the coeffcients are evaluated (j in the paper)
+        dft_grids           : int or array of int, the order(s) at which the coeffcients are evaluated (j in the paper)
         nmesh               : number of points to construct the smear Dirac function.
+    Returns:
+        Array or a float.
     """
-    dft_smear_dirac_k = _eval_dft_coeffs_smear_dirac(max_dft_order, rescaled_energy_acc, dft_grid, nmesh)
-    dft_heaviside_k = dft_coeffs_heaviside(dft_grid)
-    dft_approx_heaviside_k = np.sqrt(2 * np.pi) * dft_smear_dirac_k * dft_heaviside_k
+    grids = np.linspace(-np.pi, np.pi, nmesh + 1, endpoint=True)
+    dft_smear_dirac = _eval_dft_coeffs_smear_dirac(max_dft_order, rescaled_energy_acc, dft_grids, grids=grids, nmesh=nmesh)
+    dft_heaviside = _eval_dft_coeffs_heaviside(dft_grids)
+    dft_approx_heaviside_k = np.sqrt(2 * np.pi) * dft_smear_dirac * dft_heaviside
+    
     return dft_approx_heaviside_k
 
 
@@ -31,19 +35,19 @@ def approx_heaviside_from_dft(max_dft_order, rescaled_energy_acc, energy_grids):
     return approx_heaviside
 
 
-def approx_heaviside_from_convol(max_dft_order, rescaled_energy_acc, nmesh=200):
+def approx_heaviside_from_convol(max_dft_order, rescaled_energy_acc, nmesh=2000):
     '''
-    Approximate Heaviside function from 
+    Approximate Heaviside function from convolution of smear dirac function and periodic heaviside function.
     '''
-    # x = np.linspace(-np.pi, np.pi, nmesh + 1, endpoint=True)
-    smear_dirac = _eval_smear_dirac(max_dft_order, rescaled_energy_acc, nmesh=nmesh, grids=None)
-    heaviside = _gen_heaviside(nmesh)
+    grids = np.linspace(-np.pi, np.pi, nmesh + 1, endpoint=True)
+    smear_dirac = _eval_smear_dirac(max_dft_order, rescaled_energy_acc, nmesh=nmesh, grids=grids)
+    heaviside = _gen_heaviside(grids=grids, nmesh=nmesh)
     approx_heaviside = np.convolve(smear_dirac, heaviside, mode="same")
 
     return approx_heaviside
     
 
-def _eval_smear_dirac(max_dft_order, rescaled_energy_acc, nmesh=None, grids=None, thr=1e-5):
+def _eval_smear_dirac(max_dft_order, rescaled_energy_acc, grids=None, nmesh=None, thr=1e-5):
     """
     Evaluate smear Dirac delta function in Lemma 5 at [-pi, pi].
     Args:
@@ -64,11 +68,13 @@ def _eval_smear_dirac(max_dft_order, rescaled_energy_acc, nmesh=None, grids=None
 
     return smear_dirac
 
-def _eval_dft_coeffs_smear_dirac(max_dft_order, rescaled_energy_acc, dft_grids, nmesh, thr=1e-5):
+def _eval_dft_coeffs_smear_dirac(max_dft_order, rescaled_energy_acc, dft_grids, grids=None, nmesh=None, thr=1e-5):
     """
     Fourier transform of the smear dirac function.
     """
-    grids = np.linspace(-np.pi, np.pi, nmesh + 1, endpoint=True)
+    if grids is None:
+        assert nmesh is not None
+        grids = np.linspace(-np.pi, np.pi, nmesh + 1, endpoint=True)
     smear_dirac = _eval_smear_dirac(max_dft_order, rescaled_energy_acc, grids=grids, thr=thr)
     dft_smear_dirac = _eval_dft_coeffs_slow(smear_dirac, dft_grids, grids)
     return dft_smear_dirac
@@ -78,11 +84,12 @@ def _eval_dft_coeffs_slow(vals_func, dft_grids, grids):
     """
     Slow version of discrete Fourier transform.
     Args:
-        vals_func       : the array of the values of function f(x) to be transformed
+        vals_func  : the array of the values of function f(x) to be transformed
         dft_grid   : the grid for the recipracal space
         grid       : original space points in [-pi, pi]
     Returns:
         Fourier coefficients of f(x) at dft_grid.
+    NOTE: the number of grids has to be large enough (> 10^4) to reach decent accuracy.
     """
     
     try:  
@@ -97,7 +104,7 @@ def _eval_dft_coeffs_slow(vals_func, dft_grids, grids):
     return dft_func
 
 
-def dft_coeffs_heaviside(dft_grid):
+def _eval_dft_coeffs_heaviside(dft_grids):
     """
     FFT of the periodic Heaviside function.
     Args:
@@ -107,18 +114,18 @@ def dft_coeffs_heaviside(dft_grid):
     """
     try:
         np.seterr(divide="ignore", invalid="ignore")
-        num_order = len(dft_grid)
+        num_order = len(dft_grids)
         dft_heaviside_k = np.zeros(num_order)
-        dft_heaviside_k = -2.0j * (dft_grid % 2) / (np.sqrt(2 * np.pi) * dft_grid)  # complain when k = 0
-        dft_heaviside_k[np.where(dft_grid == 0)] = np.sqrt(np.pi / 2.0)
+        dft_heaviside_k = -2.0j * (dft_grids % 2) / (np.sqrt(2 * np.pi) * dft_grids)  # complain when k = 0
+        dft_heaviside_k[np.where(dft_grids == 0)] = np.sqrt(np.pi / 2.0)
 
     except:
-        if dft_grid == 0:
+        if dft_grids == 0:
             dft_heaviside_k = np.sqrt(np.pi / 2.0)
-        elif dft_grid % 2 == 0:
+        elif dft_grids % 2 == 0:
             dft_heaviside_k = 0
         else:
-            dft_heaviside_k = -2.0j / (np.sqrt(2 * np.pi) * dft_grid)
+            dft_heaviside_k = -2.0j / (np.sqrt(2 * np.pi) * dft_grids)
 
     return dft_heaviside_k
 
@@ -139,11 +146,17 @@ def rescale_hamiltonian_spectrum(hamiltonian, bound=np.pi/3):
     return energy_rescaling_factor
 
 
-def _gen_heaviside(nmesh):
+def _gen_heaviside(grids=None, nmesh=None):
     """
-    Return the Heaviside function in an array of s ize (nmesh + 1).
+    Return the Heaviside function in an array of size (nmesh + 1).
     """
-    heaviside_array = np.ones(nmesh + 1)
-    heaviside_array[: int(nmesh // 2)] *= 0
+    if grids is not None:
+        nmesh = len(grids)
+        heaviside_array = np.ones(nmesh)
+        heaviside_array[np.where(grids < 0)] *= 0
+    else:
+        assert nmesh is not None
+        heaviside_array = np.ones(nmesh + 1)
+        heaviside_array[: int(nmesh // 2)] *= 0
 
     return heaviside_array
--- tests/test_helpers.py
+++ tests/test_helpers.py
@@ -14,7 +14,7 @@ class TestHelpers():
         grids = np.linspace(-np.pi, np.pi, 1000)
         def sin(x):
             return np.sin(w * x)
-        fx = sin(grids)
+
         def dft_sin(k):
             if k == w:
                 tmp1 = np.pi
@@ -25,6 +25,8 @@ class TestHelpers():
             else:
                 tmp2 = np.sin(np.pi*(w + k)) /(w + k)
             return -1.j / np.sqrt(2 * np.pi) * (tmp1 - tmp2)
+        
+        fx = sin(grids)
         dft_grids = np.array([-2*w, -w, 0, w, -2*w])
         dft_f = helpers._eval_dft_coeffs_slow(fx, dft_grids, grids)
         dft_reference = []
@@ -39,38 +41,40 @@ class TestHelpers():
         d = 20
         delt = 0.2
         x = np.linspace(-np.pi, np.pi, nmesh+1, endpoint=True)
-        M = helpers._eval_smear_dirac(d, delt, nmesh)
-        
+        M = helpers._eval_smear_dirac(d, delt, grids=x)
         # make sure that the integration is 1
         area = integrate.simpson(M, x)
         assert(abs(area - 1) < 1e-5)
                 
-    def test_dft_coeffs_smear_dirac(self):
-        nmesh = 10
-        d = 20
-        delt = 0.2
-        k = np.arange(-nmesh,nmesh+1,1)
-        Mk = helpers._eval_dft_coeffs_smear_dirac(d, delt, k, nmesh)
-        print(Mk)
 
-    def test_dft_coeffs_heaviside(self):
-        k = np.arange(-4, 5)
-        Hk = helpers.dft_coeffs_heaviside(k)
-        print(Hk)
-        Hks = []
-        for i in k:
-            Hks.append(helpers.dft_coeffs_heaviside(i))
-        Hks = np.asarray(Hks)
-        print(np.linalg.norm(Hks - Hk))
-        print(k)
-
+    def test_eval_dft_coeffs_heaviside(self):
+        dft_grids = np.arange(-3, 4)
+        Hk0 = helpers._eval_dft_coeffs_heaviside(dft_grids)
+        # compare to the result from _eval_dft_coeffs_slow
+        nmesh = 30000
+        grids = np.linspace(-np.pi, np.pi, nmesh+1, endpoint=True)
+        heaviside = helpers._gen_heaviside(nmesh=nmesh)
+        Hk_slow = helpers._eval_dft_coeffs_slow(heaviside, dft_grids, grids)
+        diff = np.linalg.norm(Hk0 - Hk_slow)
+        assert diff < 1e-4
+          
+    
     def test_dft_coeffs_approx_heaviside(self):
-        nmesh = 10
+        k = 0
+        nmesh = 1000
         d = 20
         delt = 0.2
-        k = np.arange(-nmesh,nmesh+1,1)
-        Fk = helpers.dft_coeffs_approx_heaviside(d, delt, k, nmesh)
-        print(Fk)
+        ah_dft = helpers.dft_coeffs_approx_heaviside(d, delt, k, nmesh)
+        assert abs(ah_dft - np.sqrt(np.pi/2)) < 1e-10
+ 
+        k = 4
+        ah_dft = helpers.dft_coeffs_approx_heaviside(d, delt, k, nmesh)
+        assert abs(ah_dft) < 1e-10
+        
+        k = 1
+        ah_dft = helpers.dft_coeffs_approx_heaviside(d, delt, k, nmesh)
+        ah_ref = 5.536436314976699e-18-0.795265159938173j
+        assert np.linalg.norm(ah_dft - ah_ref) < 1e-10
         
 
     def test_approx_heaviside_from_dft(self):
@@ -113,4 +117,4 @@ class TestHelpers():
 
 if __name__ == "__main__":
     obj = TestHelpers()
-    obj.test_eval_dft_coeffs_slow()
+    obj.test_dft_coeffs_approx_heaviside()
